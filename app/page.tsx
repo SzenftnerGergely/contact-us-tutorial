cbeefe7b0a7c3862cfbd0f1a9c06ca3cae0f9cc1
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-11 14:10:36 +0200

    route.ts added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import ContactForm from '@/components/ContactForm'
 
 export default function Home() {
   return (
-    <div className='p-4 max-w-3xl mx-auto'>
-      <h1 className='text-3xl font-bold'>Contact Us</h1>
+    <div className='p-4 max-w-xl mx-auto'>
+      <h1 className='text-3xl font-bold my-4'>Contact Us</h1>
       <p>Please fill in the form below</p>
 
       <ContactForm/>
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,29 +1,76 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 
 export default function ContactForm() {
-  return (
-    <>
-      <form className='py-4 mt-4 border-t flex flex-col gap-5'>
-        <div className='flex flex-col gap-2'>
-            <label htmlFor="fullname">Full Name</label>
-            <input className='px-6 py-2 boder border-slate-300 shadow-md rounded-md' type="text" id='fullname' placeholder='Jhon Doe'/>
-        </div>
+    const [fullname, setFullname] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [error, setError] = useState([])
+ 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
 
-        <div className='flex flex-col gap-2'> 
-            <label htmlFor="email">Email</label>
-            <input className='px-6 py-2 boder border-slate-300 shadow-md rounded-md' type="text" id='email' placeholder='john@gmail.com' />
-        </div>
+        const res = await fetch('api/contact', {
+            method: "POST",
+            headers: {
+                "Context-type": "application/json"
+            },
+            body: JSON.stringify({
+                fullname, email, message
+            })
+        }) 
 
-        <div className='flex flex-col gap-2'>
-            <label htmlFor="message">Your Message</label>
-            <textarea className='h-32 px-6 py-2 boder border-slate-300 shadow-md rounded-md' id="message" placeholder='Type your message here'></textarea>
-        </div>
+        const {msg} = await res.json()
+        setError(msg)
+    }
 
-        <button type='submit' className='bg-green-700 p-3 text-white font-bold rounded-lg'>Send</button>
-      </form>
-      <div className='bg-slate-100 flex flex-col'>
-        <div className=' bg-red-600 px-5 py-2'>Error Message</div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <form 
+                onSubmit={handleSubmit} 
+                className='py-4 mt-4 border-t flex flex-col gap-5'
+            >
+                <div className='flex flex-col gap-2'>
+                    <label htmlFor="fullname">Full Name</label>
+                    <input
+                        className='px-6 py-2 boder border-slate-300 shadow-md rounded-md'
+                        type="text"
+                        id='fullname'
+                        placeholder='Jhon Doe'
+                        value={fullname}
+                        onChange={(e) => setFullname(e.target.value)}
+                    />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className='px-6 py-2 boder border-slate-300 shadow-md rounded-md'
+                        type="text"
+                        id='email'
+                        placeholder='john@gmail.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                    <label htmlFor="message">Your Message</label>
+                    <textarea
+                        className='h-32 px-6 py-2 boder border-slate-300 shadow-md rounded-md'
+                        id="message"
+                        placeholder='Type your message here'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                </div>
+
+                <button type='submit' className='bg-green-700 p-3 text-white font-bold rounded-lg'>Send</button>
+            </form>
+            <div className='bg-slate-100 flex flex-col'>
+                <div className=' bg-red-600 px-5 py-2'>Error Message</div>
+            </div>
+        </>
+    )
 }
